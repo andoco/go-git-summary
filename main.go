5e53d65f1dbc727a478eabe337b2788c89a47e4a
Andrew O'Connor <andrew.oconnor@gmail.com>
2019-01-04 16:23:36 +0000

    Checking all repo folders under path

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
 
 	"github.com/pkg/errors"
 	"gopkg.in/src-d/go-git.v4"
@@ -12,9 +14,47 @@ import (
 func main() {
 	path := os.Args[1]
 
-	if err := checkRepoPath(path); err != nil {
-		log.Fatal(errors.Wrapf(err, "checking repo at path", path))
+	repos, err := findRepos(path)
+	if err != nil {
+		log.Fatal(errors.Wrapf(err, "finding repos at path %s", path))
 	}
+
+	for _, p := range repos {
+		fmt.Printf("Checking repo %s\n", p)
+		if err := checkRepoPath(p); err != nil {
+			log.Fatal(errors.Wrapf(err, "checking repo at path %s", p))
+		}
+	}
+
+}
+
+// findRepos will find all the repo folders that are children of path.
+func findRepos(root string) ([]string, error) {
+	files, err := ioutil.ReadDir(root)
+	if err != nil {
+		return nil, errors.Wrapf(err, "reading dir %v", root)
+	}
+
+	repos := []string{}
+
+	for _, file := range files {
+		if file.IsDir() {
+			tmpPath := path.Join(root, file.Name())
+			if isGitRepo(tmpPath) {
+				repos = append(repos, tmpPath)
+			}
+		}
+	}
+
+	return repos, nil
+}
+
+// isGitRepo checks if the folder at path p contains a git repo.
+func isGitRepo(p string) bool {
+	if _, err := os.Stat(path.Join(p, ".git")); !os.IsNotExist(err) {
+		return true
+	}
+	return false
 }
 
 func checkRepoPath(path string) error {
